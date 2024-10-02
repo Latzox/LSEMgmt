c6a9aea6d9b6dbee7c09725fe0eec22a624ff4a8
Marco Platzer <mail@latzo.ch>
2024-10-02 14:15:04 +0200

    Remove comments

--- LSEMgmt.psm1
+++ LSEMgmt.psm1
@@ -1,9 +1,1 @@
-. $PSScriptRoot\functions\Write-Log.ps1
-# . $PSScriptRoot\functions\Get-DiskUsage.ps1
-# . $PSScriptRoot\functions\Test-InternetConnection.ps1
-# . $PSScriptRoot\functions\Invoke-ApiRequest.ps1
-# . $PSScriptRoot\functions\Invoke-SQLQuery.ps1
-# . $PSScriptRoot\functions\Set-EnvironmentVariable.ps1
-# . $PSScriptRoot\functions\Get-NetworkInfo.ps1
-# . $PSScriptRoot\functions\Test-PortAvailability.ps1
-# . $PSScriptRoot\functions\Get-OSDetails.ps1
+. $PSScriptRoot\functions\Compare-FileHash.ps1
